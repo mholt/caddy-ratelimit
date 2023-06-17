2dc0d586f0b87e983757c403bc0929ddeb84a537
Matthew Holt <mholt@users.noreply.github.com>
2023-06-17 15:01:00 -0600

    Apply license; add copyright

--- caddyfile.go
+++ caddyfile.go
@@ -1,3 +1,17 @@
+// Copyright 2021 Matthew Holt
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package caddyrl
 
 import (
@@ -23,21 +37,20 @@ func parseCaddyfile(helper httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, e
 
 // UnmarshalCaddyfile implements caddyfile.Unmarshaler. Syntax:
 //
-// rate_limit {
-//     zone <name> {
-//         key    <string>
-//         window <duration>
-//         events <max_events>
-//     }
-//     distributed {
-//         read_interval  <duration>
-//         write_interval <duration>
-//     }
-//     storage <module...>
-//     jitter  <percent>
-//     sweep_interval <duration>
-// }
-//
+//	rate_limit {
+//	    zone <name> {
+//	        key    <string>
+//	        window <duration>
+//	        events <max_events>
+//	    }
+//	    distributed {
+//	        read_interval  <duration>
+//	        write_interval <duration>
+//	    }
+//	    storage <module...>
+//	    jitter  <percent>
+//	    sweep_interval <duration>
+//	}
 func (h *Handler) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
 		if d.NextArg() {
